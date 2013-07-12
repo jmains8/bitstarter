7dc946d954567de09b010feb25631f48db6201aa
Ubuntu <ubuntu@ip-172-31-37-60.us-west-2.compute.internal>
2013-07-12 06:46:05 +0000

    updated web.js for index.html

--- web.js
+++ web.js
@@ -1,14 +1,28 @@
-var fs = required('fs');
-var express = require('express');
-var buffer = new Buffer(fs.readFileSync('index.html'), 'utf-8');
+//var fs = required('fs');
+//var express = require('express');
+//var buffer = new Buffer(fs.readFileSync('index.html'), 'utf-8');
 
+//var app = express.createServer(express.logger());
+
+//app.get('/', function(request, response) {
+//  response.send(buffer.toString()));
+//});
+
+
+
+
+var express = require('express');
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
+var content = fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-  response.send(buffer.toString()));
+  response.send(content.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
